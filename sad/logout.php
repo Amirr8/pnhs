ce5729e77694a7b83cceb01dc3cef8a43392a8bc
Amirr8 <164495490+Amirr8@users.noreply.github.com>
2025-03-05 18:18:58 +0800

    Delete sad directory

--- sad/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connection.php';
-
-session_unset();
-session_destroy();
-echo '<script>alert ("Are you sure you want to logout?") ; window.location.href = "login.php"; </script>';
-header("Location: login.php");  
-
-?>
